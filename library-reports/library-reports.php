6cebce6c9e91fde1b64da28a409b4c6099505319
michaelwright235 <maiklpolkovnikov@gmail.com>
2023-06-26 14:02:12 +0300

    Новая версия 1.3

--- library-reports/library-reports.php
+++ library-reports/library-reports.php
@@ -4,7 +4,7 @@
 Plugin Name: Library Reports
 Plugin URI: https://github.com/michaelwright235/library-reports
 Description: Ежедневная отчетность для библиотек
-Version: 1.2
+Version: 1.3
 Author: Michael Wright
 Author URI: https://github.com/michaelwright235
 License: MIT license
